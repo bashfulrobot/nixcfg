8337854b4665808b108fed07ad12ff6a2c0eaee2
bashfulrobot <dustin@bashfulrobot.com>
2024-11-25 21:38:44 -0800

    refactor: move user specific settings to user module

--- modules/desktops/gnome/default.nix
+++ modules/desktops/gnome/default.nix
@@ -12,35 +12,24 @@ in {
   config = lib.mkIf cfg.enable {
 
     services = {
-    xserver = {
-      # Enable the X11 windowing system.
-      enable = true;
-      # Enable the GNOME Desktop Environment.
-      displayManager.gdm.enable = true;
-      desktopManager.gnome.enable = true;
-      # Configure keymap in X11
-      xkb = {
-        layout = "us";
-        variant = "";
+      xserver = {
+        # Enable the X11 windowing system.
+        enable = true;
+        # Enable the GNOME Desktop Environment.
+        displayManager.gdm.enable = true;
+        desktopManager.gnome.enable = true;
+        # Configure keymap in X11
+        xkb = {
+          layout = "us";
+          variant = "";
+        };
       };
+      
     };
-    # Enable the OpenSSH daemon.
-    openssh.enable = true;
-    # Enable CUPS to print documents.
-    printing.enable = true;
-  };
-
-  # Set your time zone.
-  time.timeZone = "America/Vancouver";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_CA.UTF-8";
 
     
 
-    environment.systemPackages = with pkgs; [
-    ];
-
+    environment.systemPackages = with pkgs; [ ];
 
     # environment.gnome.excludePackages = (with pkgs; [
     #   # for packages that are pkgs.*
@@ -85,7 +74,6 @@ in {
       chown root:root /var/lib/AccountsService/icons/${user-settings.user.username}
       chmod 0444 /var/lib/AccountsService/icons/${user-settings.user.username}
     '';
-
 
     ##### Home Manager Config options #####
     # home-manager.users."${user-settings.user.username}" = {

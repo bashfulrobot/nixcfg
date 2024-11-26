0722edc6563ad194a06b929cb32b5d86e20163a8
bashfulrobot <dustin@bashfulrobot.com>
2024-11-25 22:20:03 -0800

    bug: disable ssh config

--- modules/sys/ssh/default.nix
+++ modules/sys/ssh/default.nix
@@ -20,84 +20,84 @@ in {
       programs.ssh = {
         enable = true;
 
-        extraConfig = ''
-          Host *
-            AddKeysToAgent yes
-          # Host github.com
-          #   HostName github.com
-          #   IdentityFile ~/.ssh/id_rsa_temp
-          #   User git
-          # Host bitbucket.org
-          #     HostName bitbucket.org
-          #     User git
-          Host feral
-              HostName aion.feralhosting.com
-              User msgedme
-          Host camino
-            HostName 64.225.50.102
-            User root
-            AddKeysToAgent yes
-          Host remi
-            HostName 72.51.28.133
-            User dustin
-            AddKeysToAgent yes
-          Host gigi
-            HostName 100.96.21.6
-            User dustin
-            AddKeysToAgent yes
-          Host tower-ts
-              HostName 100.89.2.33
-              User dustin
-                AddKeysToAgent yes
-          Host dt
-              HostName 192.168.169.2
-              User dustin
-                AddKeysToAgent yes
-          Host ub-ubuntubudgieorg
-              HostName 157.245.237.69
-              User dustin
-                AddKeysToAgent yes
-          Host ub-ubuntubudgieorg-nikola
-              HostName 157.245.237.69
-              User nikola
-          Host ub-ubuntubudgieorg-webpub
-              HostName 157.245.237.69
-              User webpub
-          Host ub-docker-root
-              HostName 134.209.129.108
-              User dustin
-                AddKeysToAgent yes
-          Host ub-docker-admin
-              HostName 134.209.129.108
-              User docker-admin
-                AddKeysToAgent yes
-          Host srv
-              HostName 192.168.168.1
-              User dustin
-              dKeysToAgent yes
-          Host 192.168.168.1
-              HostName 192.168.168.1
-              User dustin
-              dKeysToAgent yes
-              Port 22
-              StrictHostKeyChecking no
-              UserKnownHostsFile /dev/null
-          Host srv-ts
-                HostName 100.64.187.14
-                User dustin
-                AddKeysToAgent yes
-          Host 100.64.187.14
-                User dustin
-                AddKeysToAgent yes
-          Host nixdo
-              HostName 192.168.168.10
-              User dustin
-                AddKeysToAgent yes
-          Host rembot
-            HostName 100.89.186.70
-            User dustin
-            AddKeysToAgent yes
-        '';
+        # extraConfig = ''
+        #   Host *
+        #     AddKeysToAgent yes
+        #   # Host github.com
+        #   #   HostName github.com
+        #   #   IdentityFile ~/.ssh/id_rsa_temp
+        #   #   User git
+        #   # Host bitbucket.org
+        #   #     HostName bitbucket.org
+        #   #     User git
+        #   Host feral
+        #       HostName aion.feralhosting.com
+        #       User msgedme
+        #   Host camino
+        #     HostName 64.225.50.102
+        #     User root
+        #     AddKeysToAgent yes
+        #   Host remi
+        #     HostName 72.51.28.133
+        #     User dustin
+        #     AddKeysToAgent yes
+        #   Host gigi
+        #     HostName 100.96.21.6
+        #     User dustin
+        #     AddKeysToAgent yes
+        #   Host tower-ts
+        #       HostName 100.89.2.33
+        #       User dustin
+        #         AddKeysToAgent yes
+        #   Host dt
+        #       HostName 192.168.169.2
+        #       User dustin
+        #         AddKeysToAgent yes
+        #   Host ub-ubuntubudgieorg
+        #       HostName 157.245.237.69
+        #       User dustin
+        #         AddKeysToAgent yes
+        #   Host ub-ubuntubudgieorg-nikola
+        #       HostName 157.245.237.69
+        #       User nikola
+        #   Host ub-ubuntubudgieorg-webpub
+        #       HostName 157.245.237.69
+        #       User webpub
+        #   Host ub-docker-root
+        #       HostName 134.209.129.108
+        #       User dustin
+        #         AddKeysToAgent yes
+        #   Host ub-docker-admin
+        #       HostName 134.209.129.108
+        #       User docker-admin
+        #         AddKeysToAgent yes
+        #   Host srv
+        #       HostName 192.168.168.1
+        #       User dustin
+        #       dKeysToAgent yes
+        #   Host 192.168.168.1
+        #       HostName 192.168.168.1
+        #       User dustin
+        #       dKeysToAgent yes
+        #       Port 22
+        #       StrictHostKeyChecking no
+        #       UserKnownHostsFile /dev/null
+        #   Host srv-ts
+        #         HostName 100.64.187.14
+        #         User dustin
+        #         AddKeysToAgent yes
+        #   Host 100.64.187.14
+        #         User dustin
+        #         AddKeysToAgent yes
+        #   Host nixdo
+        #       HostName 192.168.168.10
+        #       User dustin
+        #         AddKeysToAgent yes
+        #   Host rembot
+        #     HostName 100.89.186.70
+        #     User dustin
+        #     AddKeysToAgent yes
+        # '';
       };
     };
   };

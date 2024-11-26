e54b746346811f5a373c54413f1c39df6a7e88f9
bashfulrobot <dustin@bashfulrobot.com>
2024-11-25 21:45:21 -0800

    refactor: merge digdug files

--- systems/digdug/default.nix
+++ systems/digdug/default.nix
@@ -8,6 +8,23 @@
     # ../../suites/autoimport.nix
   ];
 
+  programs = {
+    # Install firefox.
+    firefox.enable = true;
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # mtr.enable = true;
+    # gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
+
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [ git git-crypt neovim ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave

e8079736be105efa528d06020959c502462e14b1
bashfulrobot <dustin@bashfulrobot.com>
2024-11-26 14:37:34 -0800

    feat: Enable firmware updates and add hardware kernel modules
    - Import the `not-detected.nix` file in `hardware-configuration.nix`
    - Modify several kernel module configurations in `hardware-configuration.nix`
    - Enable DHCP on ethernet and wireless interfaces
    - Set `nixpkgs.hostPlatform` to `"x86_64-linux"`
    - Modify the `updateMicrocode` in `hardware.cpu.amd`
    - Add a `default.nix` file for hardware firmware configuration
    - Enable firmware updates and fwupd service for firmware updates
    - Include an empty `environment.systemPackages` list in `default.nix`
    - Add various kernel modules in `hardware-configuration.nix`
    - Set `kvm-intel` as a kernel module
    - Remove commented out section for Sway reload in `justfile`
    - Remove exporting plasma config to Nix in `justfile`
    - Make a change to the `kernel` recipe to include `ls` command with `sudo`
    - Enable workstation archetype with several features enabled in `default.nix`
    
    [systems/qbert/config/hardware-configuration.nix]
    - Import the file `modulesPath + "/installer/scan/not-detected.nix"`
    - Modify the `availableKernelModules` in `boot.initrd`
    - Modify the `kernelModules` in `boot.initrd`
    - Modify the `kernelModules` in `boot`
    - Modify the `extraModulePackages` in `boot`
    - Enable DHCP on each ethernet and wireless interface
    - Set `nixpkgs.hostPlatform` to `"x86_64-linux"`
    - Modify the `updateMicrocode` in `hardware.cpu.amd`
    
    Note: The summary does not include comments that simply repeat the contents of the file.
    [modules/hw/firmware/default.nix]
    - Add `default.nix` file for hardware firmware configuration
    - Enable firmware updates
    - Enable fwupd service for firmware updates
    - Include empty `environment.systemPackages` list
    [systems/digdug/config/hardware-configuration.nix]
    - Add the `thunderbolt`, `nvme`, `usb_storage`, `sd_mod`, and `rtsx_pci_sdmmc` kernel modules
    - Set `kvm-intel` as a kernel module
    - Enable DHCP on all ethernet and wireless interfaces
    [justfile]
    - Remove a commented out section for Sway reload
    - Remove exporting plasma config to Nix
    - Make a change to the `kernel` recipe to include `sudo` for the `ls` command
    [archetype/workstation/default.nix]
    - Enable workstation archetype
    - Enable home-manager, insecure-packages, and nix-settings
    - Enable printing service
    - Enable dev suite and offcoms, terminal utilities
    - Enable dconf, fonts, and ssh in sys module
    - Enable hw audio and firmware

--- systems/digdug/config/hardware-configuration.nix
+++ systems/digdug/config/hardware-configuration.nix
@@ -4,14 +4,24 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "xhci_pci"
+        "thunderbolt"
+        "nvme"
+        "usb_storage"
+        "sd_mod"
+        "rtsx_pci_sdmmc"
+      ];
+      kernelModules = [ ];
+    };
+
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -21,5 +31,6 @@
   # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }

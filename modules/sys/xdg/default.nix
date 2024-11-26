e8a9282d77afbc118e292b47d1ec0f87cdbfbce1
bashfulrobot <dustin@bashfulrobot.com>
2024-11-26 12:50:50 -0800

    feat: Enable the GNOME Desktop, X11, GDM, keymap, dconf, xdg, keybindings, and user settings
    - Enabled the GNOME Desktop, X11 windowing system, GDM, and GNOME Desktop Manager
    - Configured keymap in X11 and added system packages
    - Added activation scripts for AccountsService and enabled dconf and xdg in sys
    - Enabled GNOME keybindings and added Home Manager Config options for user settings
    - Created a new file called `default.nix` in the `modules/sys/xdg` directory
    - Added a new option `sys.xdg.enable` to enable XDG and set it to `false` by default
    - Added the `xdg` configuration block with enable options for `mime`, `autostart`, `icons`, `menus`, and `sounds`
    - Configured `mimeApps` with associations and default applications
    
    [modules/desktops/gnome/default.nix]
    - Enable the GNOME Desktop
    - Enable the X11 windowing system
    - Enable the GNOME Display Manager (GDM)
    - Enable the GNOME Desktop Manager
    - Configure keymap in X11
    - Add system packages
    - Add activation scripts for AccountsService
    - Enable dconf and xdg in sys
    - Enable GNOME keybindings
    - Add Home Manager Config options for user settings
    [modules/sys/xdg/default.nix]
    - Create a new file called `default.nix` in the `modules/sys/xdg` directory
    - Add a new option `sys.xdg.enable` to enable XDG
    - Set `sys.xdg.enable` to `false` by default
    - Add the `xdg` configuration block with various enable options
    - Enable `mime`, `autostart`, `icons`, `menus`, and `sounds`
    - Configure `mimeApps` with associations and default applications

---
+++ modules/sys/xdg/default.nix
@@ -0,0 +1,59 @@
+{ user-settings, pkgs, config, lib, ... }:
+let cfg = config.sys.xdg;
+in {
+
+  options = {
+    sys.xdg.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable xdg.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ xdg-utils ];
+    # Enable xdg
+    xdg = {
+      mime.enable = true;
+      autostart.enable = true;
+      icons.enable = true;
+      menus.enable = true;
+      sounds.enable = true;
+    };
+
+    home-manager.users."${user-settings.user.username}" = {
+      xdg = {
+        # TODO: remeber, forcing the file to be overwritten when home-manager is run
+        configFile."mimeapps.list".force = true;
+
+        # desktopEntries.librewolf = {
+        #   name = "LibreWolf";
+        #   exec = "${pkgs.librewolf}/bin/librewolf";
+        # };
+        mimeApps = {
+          associations = {
+            # added = { "x-scheme-handler/tg" = "org.telegram.desktop.desktop"; };
+          };
+          enable = false;
+          defaultApplications = {
+            "text/html" = [ "com.github.cassidyjames.ephemeral.desktop" ];
+            "text/plain" = [ "code.desktop" ];
+            # "inode/directory" = [ "lf.desktop" ];
+            # "application/pdf" = [ "okular.desktop" ];
+            "applications/x-www-browser" = [ "com.github.cassidyjames.ephemeral.desktop" ];
+            "x-scheme-handler/about" = [ "com.github.cassidyjames.ephemeral.desktop" ];
+            "x-scheme-handler/http" = [ "com.github.cassidyjames.ephemeral.desktop" ];
+            "x-scheme-handler/https" = [ "com.github.cassidyjames.ephemeral.desktop" ];
+            "x-scheme-handler/mailto" = [ "com.github.cassidyjames.ephemeral.desktop" ];
+            "x-scheme-handler/msteams" = [ "teams-for-linux.desktop" ];
+            "x-scheme-handler/postman" = [ "Postman.desktop" ];
+            "x-scheme-handler/unknown" = [ "com.github.cassidyjames.ephemeral.desktop" ];
+            "x-scheme-handler/slack" = [ "slack.desktop" ];
+            # "x-scheme-handler/tg" = [ "org.telegram.desktop.desktop" ];
+            "x-scheme-handler/webcal" = [ "com.github.cassidyjames.ephemeral.desktop" ];
+          };
+        };
+      };
+    };
+  };
+}

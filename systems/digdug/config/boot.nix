5f49068bf5f0e20ca3d3a9f2721071bb949b9b3a
bashfulrobot <dustin@bashfulrobot.com>
2024-12-01 12:17:51 -0800

    refactor: update boot settings for digdug and qbert, disable kernel.nix for qbert
    Changes to be committed:
    	modified:   systems/digdug/config/boot.nix
    	modified:   systems/qbert/config/boot.nix
    	new file:   systems/qbert/config/disabled/kernel.nix

--- systems/digdug/config/boot.nix
+++ systems/digdug/config/boot.nix
@@ -1,5 +1,18 @@
 { config, pkgs, ... }: {
+
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    kernelParams = [ "quiet" "splash" ];
+    loader = {
+      efi.canTouchEfiVariables = true;
+      systemd-boot = {
+        enable = true;
+        consoleMode = "max"; # ensure windows will be found in boot menu
+        netbootxyz.enable = false;
+        # Maximum number of latest generations in the boot menu. Useful to prevent boot partition running out of disk space.
+        configurationLimit = 5;
+      };
+    };
+  };
+
 }

762a161c70b63139629cd1838a7c0e23b3f96136
bashfulrobot <dustin@bashfulrobot.com>
2024-12-03 21:27:54 -0800

    feat: Enable power management and TLP service in sys.power module
    - Enable power management in `systems/digdug/config/modules.nix`
    - Add `modules/sys/power/default.nix`
    - Define `sys.power.enable` option with default value `false` and a description
    - Enable power management and TLP service in the `config` section of `modules/sys/power/default.nix`
    - Comment out the `home-manager.users` section in `modules/sys/power/default.nix`
    
    [systems/digdug/config/modules.nix]
    - Enable power management
    [modules/sys/power/default.nix]
    - Added a new file `default.nix` in the `modules/sys/power` directory
    - Defined an option `sys.power.enable` with a default value of `false` and a description in the `options` section
    - Enabled power management and TLP service in the `config` section
    - Commented out the `home-manager.users` section

---
+++ modules/sys/power/default.nix
@@ -0,0 +1,23 @@
+{ user-settings, pkgs, config, lib, ... }:
+let cfg = config.sys.power;
+in {
+
+  options = {
+    sys.power.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable laptop power tools.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    # environment.systemPackages = with pkgs; [  ];
+    powerManagement.enable = true;
+
+    services.tlp.enable = true;
+
+    # home-manager.users."${user-settings.user.username}" = {
+
+    # };
+  };
+}

d955e6f081a0da14af98cec3ad59adc781f139be
bashfulrobot <dustin@bashfulrobot.com>
2024-12-03 21:27:54 -0800

    feat: Enable power management and TLP service in sys.power module
    - Enable power management in `systems/digdug/config/modules.nix`
    - Add `modules/sys/power/default.nix`
    - Define `sys.power.enable` option with default value `false` and a description
    - Enable power management and TLP service in the `config` section of `modules/sys/power/default.nix`
    - Comment out the `home-manager.users` section in `modules/sys/power/default.nix`
    
    [systems/digdug/config/modules.nix]
    - Enable power management
    [modules/sys/power/default.nix]
    - Added a new file `default.nix` in the `modules/sys/power` directory
    - Defined an option `sys.power.enable` with a default value of `false` and a description in the `options` section
    - Enabled power management and TLP service in the `config` section
    - Commented out the `home-manager.users` section

--- systems/digdug/config/modules.nix
+++ systems/digdug/config/modules.nix
@@ -5,6 +5,9 @@
   # Enable gnome desktop
   desktops.gnome.enable = true;
 
+  # Enable power management
+  sys.power.enable = true;
+
   environment.systemPackages = with pkgs; [ mullvad ];
 
   services.mullvad-vpn.enable = true;

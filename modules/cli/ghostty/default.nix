14c32dd5b7173e8a933598d303889ce259fe7236
bashfulrobot <dustin@bashfulrobot.com>
2024-12-31 12:39:33 -0800

    feat: Add configuration options for Ghostty terminal in CLI.
    - Enabled Ghostty terminal in the CLI configuration
    - Added system packages for Ghostty
    - Set various Ghostty configuration options such as font size, cursor style, window theme, fullscreen mode, working directory, custom shader animation, clipboard access, window padding, background color, cursor blink, and mouse hide-while-typing
    
    [modules/cli/ghostty/default.nix]
    - Enable Ghostty terminal in the CLI configuration
    - Add system packages for Ghostty
    - Set Ghostty configuration options for font size, cursor style, window theme, fullscreen mode, working directory, custom shader animation, clipboard access, window padding, background color, cursor blink, and mouse hide-while-typing

--- modules/cli/ghostty/default.nix
+++ modules/cli/ghostty/default.nix
@@ -10,20 +10,28 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      inputs.ghostty.packages.x86_64-linux.default
-     ];
+    environment.systemPackages = with pkgs;
+      [ inputs.ghostty.packages.x86_64-linux.default ];
 
     home-manager.users."${user-settings.user.username}" = {
-    # https://ghostty.org/docs/config
-        home.file.".config/ghostty/config".text = ''
-          font-size = 16
-          cursor-style = block
-          mouse-hide-while-typing = true
-          window-padding-x = 10
-          window-padding-balance = true
-          window-theme = system
-        '';
+      # https://ghostty.org/docs/config
+      home.file.".config/ghostty/config".text = ''
+        font-size = 16
+        cursor-style = block
+        window-theme = system
+        fullscreen = true
+        working-directory = ~/dev
+        custom-shader-animation = true
+        clipboard-read = allow
+        clipboard-write = allow
+        window-padding-x = 10
+        window-padding-y = 10
+        window-padding-balance = true
+        window-padding-color = extend-always
+        background = #1F1F1F
+        cursor-style-blink = true
+        mouse-hide-while-typing = true
+      '';
     };
   };
 }

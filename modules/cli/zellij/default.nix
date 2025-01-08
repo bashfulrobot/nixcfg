58ba88c16f5c4f3f3071cc09a199cd9ffaceb3ec
bashfulrobot <dustin@bashfulrobot.com>
2025-01-08 12:06:25 -0800

    feat: Enable and configure the `zellij` terminal program
    [suites/terminal/default.nix]
    - Enable the `zellij` and `yazi` programs in the terminal suite
    [modules/cli/zellij/default.nix]
    - Add a new file `default.nix` in the `modules/cli/zellij` directory
    - Enable the `zellij` CLI tool in the configuration
    - Configure `zellij` settings like scrollback editor, copy command, default shell, and pane frames

---
+++ modules/cli/zellij/default.nix
@@ -0,0 +1,35 @@
+{ user-settings, pkgs, secrets, config, lib, ... }:
+let cfg = config.cli.zellij;
+in {
+  options = {
+    cli.zellij.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable zellij.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    # environment.systemPackages = with pkgs; [  ];
+
+    home-manager.users."${user-settings.user.username}" = {
+
+      programs.zellij = {
+        enable = true;
+        enableBashIntegration = true;
+        enableFishIntegration = true;
+        settings = {
+          scrollback_editor = "nvim";
+          copy_command = "wl-copy";
+          default_shell = lib.getExe pkgs.fish;
+          scroll_buffer_size = 50000;
+          ui = {
+            pane_frames.rounded_corners = true;
+            ane_frames.hide_session_name = true;
+          };
+        };
+      };
+
+    };
+  };
+}

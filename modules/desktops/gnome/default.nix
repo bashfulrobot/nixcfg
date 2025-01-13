d2690be2f5f43d73dd200cdaaa3de99db0e02b7a
bashfulrobot <dustin@bashfulrobot.com>
2025-01-13 09:29:26 -0800

    feat: Update Tiling Shell configuration settings
    [modules/desktops/gnome/default.nix]
    - Update layouts in tiling shell
    - Added 3x vertical layout
    - Added 4x vertical layout

--- modules/desktops/gnome/default.nix
+++ modules/desktops/gnome/default.nix
@@ -160,21 +160,44 @@ in {
         "org/gnome/shell/extensions/tilingshell" = {
           enable-blur-selected-tilepreview = true;
           enable-blur-snap-assistant = true;
+          inner-gaps = mkUint32 16;
           last-version-name-installed = "14";
           layouts-json = ''
-            [{"id":"Layout 1","tiles":[{"x":0,"y":0,"width":0.22,"height":0.5,"groups":[1,2]},{"x":0,"y":0.5,"width":0.22,"height":0.5,"groups":[1,2]},{"x":0.22,"y":0,"width":0.56,"height":1,"groups":[2,3]},{"x":0.78,"y":0,"width":0.22,"height":0.5,"groups":[3,4]},{"x":0.78,"y":0.5,"width":0.22,"height":0.5,"groups":[3,4]}]},{"id":"Layout 2","tiles":[{"x":0,"y":0,"width":0.22,"height":1,"groups":[1]},{"x":0.22,"y":0,"width":0.56,"height":1,"groups":[1,2]},{"x":0.78,"y":0,"width":0.22,"height":1,"groups":[2]}]},{"id":"Layout 3","tiles":[{"x":0,"y":0,"width":0.33,"height":1,"groups":[1]},{"x":0.33,"y":0,"width":0.67,"height":1,"groups":[1]}]},{"id":"Layout 4","tiles":[{"x":0,"y":0,"width":0.67,"height":1,"groups":[1]},{"x":0.67,"y":0,"width":0.33,"height":1,"groups":[1]}]},{"id":"3722439","tiles":[{"x":0,"y":0,"width":0.35358796296296297,"height":0.49234449760765553,"groups":[1,2]},{"x":0.35358796296296297,"y":0,"width":0.6464120370370365,"height":1,"groups":[1]},{"x":0,"y":0.49234449760765553,"width":0.35358796296296297,"height":0.5076555023923448,"groups":[2,1]}]}]'';
+            [{"id":"Layout 1","tiles":[{"x":0,"y":0,"width":0.22,"height":0.5,"groups":[1,2]},{"x":0,"y":0.5,"width":0.22,"height":0.5,"groups":[1,2]},{"x":0.22,"y":0,"width":0.56,"height":1,"groups":[2,3]},{"x":0.78,"y":0,"width":0.22,"height":0.5,"groups":[3,4]},{"x":0.78,"y":0.5,"width":0.22,"height":0.5,"groups":[3,4]}]},{"id":"Layout 2","tiles":[{"x":0,"y":0,"width":0.22,"height":1,"groups":[1]},{"x":0.22,"y":0,"width":0.56,"height":1,"groups":[1,2]},{"x":0.78,"y":0,"width":0.22,"height":1,"groups":[2]}]},{"id":"Layout 3","tiles":[{"x":0,"y":0,"width":0.33,"height":1,"groups":[1]},{"x":0.33,"y":0,"width":0.67,"height":1,"groups":[1]}]},{"id":"Layout 4","tiles":[{"x":0,"y":0,"width":0.67,"height":1,"groups":[1]},{"x":0.67,"y":0,"width":0.33,"height":1,"groups":[1]}]},{"id":"3722439","tiles":[{"x":0,"y":0,"width":0.35358796296296297,"height":0.49234449760765553,"groups":[1,2]},{"x":0.35358796296296297,"y":0,"width":0.6464120370370365,"height":1,"groups":[1]},{"x":0,"y":0.49234449760765553,"width":0.35358796296296297,"height":0.5076555023923448,"groups":[2,1]}]},{"id":"880357","tiles":[{"x":0,"y":0,"width":0.2502906976744186,"height":1,"groups":[2]},{"x":0.5002906976744186,"y":0,"width":0.24970930232558142,"height":1,"groups":[3,1]},{"x":0.2502906976744186,"y":0,"width":0.25,"height":1,"groups":[1,2]},{"x":0.75,"y":0,"width":0.25,"height":1,"groups":[3]}]},{"id":"942750","tiles":[{"x":0,"y":0,"width":0.3,"height":1,"groups":[1]},{"x":0.3,"y":0,"width":0.3511627906976745,"height":1,"groups":[2,1]},{"x":0.6511627906976745,"y":0,"width":0.3488372093023255,"height":1,"groups":[2]}]}]'';
+          outer-gaps = mkUint32 8;
           overridden-settings = ''
             {"org.gnome.mutter.keybindings":{"toggle-tiled-right":"['<Super>Right']","toggle-tiled-left":"['<Super>Left']"},"org.gnome.desktop.wm.keybindings":{"maximize":"['<Super>Up']","unmaximize":"['<Super>Down', '<Alt>F5']"},"org.gnome.mutter":{"edge-tiling":"false"}}'';
           quarter-tiling-threshold = mkUint32 41;
+          selected-layouts =
+            [ [ "Layout 1" ] [ "Layout 1" ] [ "Layout 1" ] [ "Layout 1" ] ];
           show-indicator = false;
           snap-assistant-threshold = 57;
+          span-window-down = [ "<Control><Super>Down" ];
+          span-window-left = [ "<Control><Super>Left" ];
+          span-window-right = [ "<Control><Super>Right" ];
+          span-window-up = [ "<Control><Super>Up" ];
           tiling-system-activation-key = [ "0" ];
           top-edge-maximize = true;
-          span-window-right = [ "<Control><Super>Right" ];
-          span-window-left = [ "<Control><Super>Left" ];
-          span-window-up = [ "<Control><Super>Up" ];
-          span-window-down = [ "<Control><Super>Down" ];
         };
+
+        # {
+        #   enable-blur-selected-tilepreview = true;
+        #   enable-blur-snap-assistant = true;
+        #   last-version-name-installed = "14";
+        #   layouts-json = ''
+        #     [{"id":"Layout 1","tiles":[{"x":0,"y":0,"width":0.22,"height":0.5,"groups":[1,2]},{"x":0,"y":0.5,"width":0.22,"height":0.5,"groups":[1,2]},{"x":0.22,"y":0,"width":0.56,"height":1,"groups":[2,3]},{"x":0.78,"y":0,"width":0.22,"height":0.5,"groups":[3,4]},{"x":0.78,"y":0.5,"width":0.22,"height":0.5,"groups":[3,4]}]},{"id":"Layout 2","tiles":[{"x":0,"y":0,"width":0.22,"height":1,"groups":[1]},{"x":0.22,"y":0,"width":0.56,"height":1,"groups":[1,2]},{"x":0.78,"y":0,"width":0.22,"height":1,"groups":[2]}]},{"id":"Layout 3","tiles":[{"x":0,"y":0,"width":0.33,"height":1,"groups":[1]},{"x":0.33,"y":0,"width":0.67,"height":1,"groups":[1]}]},{"id":"Layout 4","tiles":[{"x":0,"y":0,"width":0.67,"height":1,"groups":[1]},{"x":0.67,"y":0,"width":0.33,"height":1,"groups":[1]}]},{"id":"3722439","tiles":[{"x":0,"y":0,"width":0.35358796296296297,"height":0.49234449760765553,"groups":[1,2]},{"x":0.35358796296296297,"y":0,"width":0.6464120370370365,"height":1,"groups":[1]},{"x":0,"y":0.49234449760765553,"width":0.35358796296296297,"height":0.5076555023923448,"groups":[2,1]}]}]'';
+        #   overridden-settings = ''
+        #     {"org.gnome.mutter.keybindings":{"toggle-tiled-right":"['<Super>Right']","toggle-tiled-left":"['<Super>Left']"},"org.gnome.desktop.wm.keybindings":{"maximize":"['<Super>Up']","unmaximize":"['<Super>Down', '<Alt>F5']"},"org.gnome.mutter":{"edge-tiling":"false"}}'';
+        #   quarter-tiling-threshold = mkUint32 41;
+        #   show-indicator = false;
+        #   snap-assistant-threshold = 57;
+        #   tiling-system-activation-key = [ "0" ];
+        #   top-edge-maximize = true;
+        #   span-window-right = [ "<Control><Super>Right" ];
+        #   span-window-left = [ "<Control><Super>Left" ];
+        #   span-window-up = [ "<Control><Super>Up" ];
+        #   span-window-down = [ "<Control><Super>Down" ];
+        # };
 
         "org/gnome/shell/extensions/bluetooth-quick-connect" = {
           bluetooth-auto-power-on = true;

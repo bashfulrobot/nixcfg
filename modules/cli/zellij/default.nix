767e563ef4a29f33b53d16faf924f9d38b76449c
bashfulrobot <dustin@bashfulrobot.com>
2025-01-14 11:09:33 -0800

    feat: Refactor Zellij CLI configuration and enable integrations
    - Added a new file `modules/cli/zellij/old`
    - Added a long piece of code with configuration options for the Zellij CLI tool and plugins
    - Set various keybindings for functions in the CLI tool
    - Updated the source of the zellij_forgot plugin to a new release
    - Made several changes to the configuration of Zellij in the `default.nix` file
    
    [modules/cli/zellij/old]
    - Added a new file `modules/cli/zellij/old`
    - Added a long piece of code that includes configuration options for the Zellij CLI tool and plugins
    - The code includes keybindings for various functions
    - Added a remote URL for fetching a Zellij plugin wasm file
    - The sha256 checksum for the fetched plugin wasm file is provided
    [modules/cli/zellij/default.nix]
    - Enable zellij in the configuration
    - Enable Bash and Fish integration
    - Set the scrollback_editor to "nvim"
    - Set the copy_command to "xclip -selection clipboard"
    - Set the default_shell to the executable for Fish
    - Set the scroll_buffer_size to 50000
    - Enable rounded_corners for pane_frames in the UI
    - Hide the session name in pane_frames
    - Set the keybinds for various actions in zellij (including float, lock, unlock, new pane, close pane, etc.)
    - Update the source of the zellij_forgot plugin to a new release

--- modules/cli/zellij/default.nix
+++ modules/cli/zellij/default.nix
@@ -19,164 +19,97 @@ in {
         enable = true;
         enableBashIntegration = true;
         enableFishIntegration = true;
-      };
+        settings = {
+          scrollback_editor = "nvim";
+          copy_command = "xclip -selection clipboard";
+          default_shell = lib.getExe pkgs.fish;
+          scroll_buffer_size = 50000;
+          # TODO: Until I can figure out: https://github.com/karimould/zellij-forgot/issues/11
+          # default_layout = "compact";
+          ui = {
+            pane_frames.rounded_corners = true;
+            pane_frames.hide_session_name = true;
+          };
+          # TODO: Until I can figure out: https://github.com/karimould/zellij-forgot/issues/11
+          # keybinds = {
+          #   "shared_except \"locked\"" = {
+          #     "bind \"Ctrl y\"" = {
+          #       LaunchOrFocusPlugin = [
+          #         "file:~/.config/zellij/plugins/zellij_forgot.wasm"
+          #       ];
+          #     };
+          #   };
 
-      home.file = {
-        ".config/zellij/config.kdl".text = ''
-              scrollback_editor "nvim"
-              copy_command "xclip -selection clipboard"
-              default_shell "${lib.getExe pkgs.fish}"
-              scroll_buffer_size 50000
-              default_layout "compact"
-              ui {
-                pane_frames {
-                  rounded_corners true
-                  hide_session_name true
-                }
-              }
-              keybinds {
-                shared_except "locked" {
-                  bind "Ctrl y" {
-                    LaunchOrFocusPlugin "file:~/.config/zellij/plugins/zellij_forgot.wasm" {
-                    "lock"                  "ctrl + g"
-                    "unlock"                "ctrl + g"
-                    "new pane"              "ctrl + p + n"
-                    "change focus of pane"  "ctrl + p + arrow key"
-                    "close pane"            "ctrl + p + x"
-                    "rename pane"           "ctrl + p + c"
-                    "toggle fullscreen"     "ctrl + p + f"
-                    "toggle floating pane"  "ctrl + p + w"
-                    "toggle embed pane"     "ctrl + p + e"
-                    "choose right pane"     "ctrl + p + l"
-                    "choose left pane"      "ctrl + p + r"
-                    "choose upper pane"     "ctrl + p + k"
-                    "choose lower pane"     "ctrl + p + j"
-                    "new tab"               "ctrl + t + n"
-                    "close tab"             "ctrl + t + x"
-                    "change focus of tab"   "ctrl + t + arrow key"
-                    "rename tab"            "ctrl + t + r"
-                    "sync tab"              "ctrl + t + s"
-                    "brake pane to new tab" "ctrl + t + b"
-                    "brake pane left"       "ctrl + t + ["
-                    "brake pane right"      "ctrl + t + ]"
-                    "toggle tab"            "ctrl + t + tab"
-                    "increase pane size"    "ctrl + n + +"
-                    "decrease pane size"    "ctrl + n + -"
-                    "increase pane top"     "ctrl + n + k"
-                    "increase pane right"   "ctrl + n + l"
-                    "increase pane bottom"  "ctrl + n + j"
-                    "increase pane left"    "ctrl + n + h"
-                    "decrease pane top"     "ctrl + n + K"
-                    "decrease pane right"   "ctrl + n + L"
-                    "decrease pane bottom"  "ctrl + n + J"
-                    "decrease pane left"    "ctrl + n + H"
-                    "move pane to top"      "ctrl + h + k"
-                    "move pane to right"    "ctrl + h + l"
-                    "move pane to bottom"   "ctrl + h + j"
-                    "move pane to left"     "ctrl + h + h"
-                    "search"                "ctrl + s + s"
-                    "go into edit mode"     "ctrl + s + e"
-                    "detach session"        "ctrl + o + w"
-                    "open session manager"  "ctrl + o + w"
-                    "quit zellij"           "ctrl + q"
-                    floating true
-                    }
-                }
-            }
-          }
-        '';
+          # };
 
-        ".config/zellij/plugins/zellij_forgot.wasm".source = pkgs.fetchurl {
-          url =
-            "https://github.com/karimould/zellij-forgot/releases/download/0.4.1/zellij_forgot.wasm";
-          sha256 = "1pxwy5ld3affpzf20i1zvn3am12vs6jwp06wbshw4g1xw8drj4ch";
         };
       };
+      home.file = {
 
-      # programs.zellij = {
-      #   enable = true;
-      #   enableBashIntegration = true;
-      #   enableFishIntegration = true;
-      #   settings = {
-      #     scrollback_editor = "nvim";
-      #     copy_command = "xclip -selection clipboard";
-      #     default_shell = lib.getExe pkgs.fish;
-      #     scroll_buffer_size = 50000;
-      #     # default_layout = "compact";
-      #     ui = {
-      #       pane_frames.rounded_corners = true;
-      #       pane_frames.hide_session_name = true;
-      #     };
-      #
-      #   };
-      # };
-
-      # xdg.configFile = {
-      #   "zellij/plugins/zellij_forgot.wasm".source = pkgs.fetchurl {
-      #     url =
-      #       "https://github.com/karimould/zellij-forgot/releases/download/0.4.1/zellij_forgot.wasm";
-      #     sha256 = "1pxwy5ld3affpzf20i1zvn3am12vs6jwp06wbshw4g1xw8drj4ch";
-      #   };
-
-      #   "zellij/keybindings.kdl".text = ''
-      #     keybinds {
-      #         shared_except {
-      #             locked {
-      #                 bind {
-      #                     "Ctrl y" {
-      #                         LaunchOrFocusPlugin {
-      #                             plugin = "file:~/zellij-plugins/zellij_forgot.wasm"
-      #                             lock = "ctrl + g"
-      #                             unlock = "ctrl + g"
-      #                             "new pane" = "ctrl + p + n"
-      #                             "change focus of pane" = "ctrl + p + arrow key"
-      #                             "close pane" = "ctrl + p + x"
-      #                             "rename pane" = "ctrl + p + c"
-      #                             "toggle fullscreen" = "ctrl + p + f"
-      #                             "toggle floating pane" = "ctrl + p + w"
-      #                             "toggle embed pane" = "ctrl + p + e"
-      #                             "choose right pane" = "ctrl + p + l"
-      #                             "choose left pane" = "ctrl + p + r"
-      #                             "choose upper pane" = "ctrl + p + k"
-      #                             "choose lower pane" = "ctrl + p + j"
-      #                             "new tab" = "ctrl + t + n"
-      #                             "close tab" = "ctrl + t + x"
-      #                             "change focus of tab" = "ctrl + t + arrow key"
-      #                             "rename tab" = "ctrl + t + r"
-      #                             "sync tab" = "ctrl + t + s"
-      #                             "brake pane to new tab" = "ctrl + t + b"
-      #                             "brake pane left" = "ctrl + t + ["
-      #                             "brake pane right" = "ctrl + t + ]"
-      #                             "toggle tab" = "ctrl + t + tab"
-      #                             "increase pane size" = "ctrl + n + +"
-      #                             "decrease pane size" = "ctrl + n + -"
-      #                             "increase pane top" = "ctrl + n + k"
-      #                             "increase pane right" = "ctrl + n + l"
-      #                             "increase pane bottom" = "ctrl + n + j"
-      #                             "increase pane left" = "ctrl + n + h"
-      #                             "decrease pane top" = "ctrl + n + K"
-      #                             "decrease pane right" = "ctrl + n + L"
-      #                             "decrease pane bottom" = "ctrl + n + J"
-      #                             "decrease pane left" = "ctrl + n + H"
-      #                             "move pane to top" = "ctrl + h + k"
-      #                             "move pane to right" = "ctrl + h + l"
-      #                             "move pane to bottom" = "ctrl + h + j"
-      #                             "move pane to left" = "ctrl + h + h"
-      #                             search = "ctrl + s + s"
-      #                             "go into edit mode" = "ctrl + s + e"
-      #                             "detach session" = "ctrl + o + w"
-      #                             "open session manager" = "ctrl + o + w"
-      #                             "quit zellij" = "ctrl + q"
-      #                             floating = true
-      #                         }
-      #                     }
-      #                 }
-      #             }
-      #         }
-      #     }
-      #   '';
-      # };
-
+        # ".config/zellij/plugins/zellij_forgot.wasm".source = pkgs.fetchurl {
+        #   url =
+        #     "https://github.com/karimould/zellij-forgot/releases/download/0.4.1/zellij_forgot.wasm";
+        #   sha256 = "1pxwy5ld3affpzf20i1zvn3am12vs6jwp06wbshw4g1xw8drj4ch";
+        # };
+        # ".config/zellij/keybindings.kdl".text = ''
+        #   keybinds {
+        #       shared_except {
+        #           locked {
+        #               bind {
+        #                   "Ctrl y" {
+        #                       LaunchOrFocusPlugin {
+        #                           plugin = "file:~/zellij-plugins/zellij_forgot.wasm"
+        #                           lock = "ctrl + g"
+        #                           unlock = "ctrl + g"
+        #                           "new pane" = "ctrl + p + n"
+        #                           "change focus of pane" = "ctrl + p + arrow key"
+        #                           "close pane" = "ctrl + p + x"
+        #                           "rename pane" = "ctrl + p + c"
+        #                           "toggle fullscreen" = "ctrl + p + f"
+        #                           "toggle floating pane" = "ctrl + p + w"
+        #                           "toggle embed pane" = "ctrl + p + e"
+        #                           "choose right pane" = "ctrl + p + l"
+        #                           "choose left pane" = "ctrl + p + r"
+        #                           "choose upper pane" = "ctrl + p + k"
+        #                           "choose lower pane" = "ctrl + p + j"
+        #                           "new tab" = "ctrl + t + n"
+        #                           "close tab" = "ctrl + t + x"
+        #                           "change focus of tab" = "ctrl + t + arrow key"
+        #                           "rename tab" = "ctrl + t + r"
+        #                           "sync tab" = "ctrl + t + s"
+        #                           "brake pane to new tab" = "ctrl + t + b"
+        #                           "brake pane left" = "ctrl + t + ["
+        #                           "brake pane right" = "ctrl + t + ]"
+        #                           "toggle tab" = "ctrl + t + tab"
+        #                           "increase pane size" = "ctrl + n + +"
+        #                           "decrease pane size" = "ctrl + n + -"
+        #                           "increase pane top" = "ctrl + n + k"
+        #                           "increase pane right" = "ctrl + n + l"
+        #                           "increase pane bottom" = "ctrl + n + j"
+        #                           "increase pane left" = "ctrl + n + h"
+        #                           "decrease pane top" = "ctrl + n + K"
+        #                           "decrease pane right" = "ctrl + n + L"
+        #                           "decrease pane bottom" = "ctrl + n + J"
+        #                           "decrease pane left" = "ctrl + n + H"
+        #                           "move pane to top" = "ctrl + h + k"
+        #                           "move pane to right" = "ctrl + h + l"
+        #                           "move pane to bottom" = "ctrl + h + j"
+        #                           "move pane to left" = "ctrl + h + h"
+        #                           search = "ctrl + s + s"
+        #                           "go into edit mode" = "ctrl + s + e"
+        #                           "detach session" = "ctrl + o + w"
+        #                           "open session manager" = "ctrl + o + w"
+        #                           "quit zellij" = "ctrl + q"
+        #                           floating = true
+        #                           "LOAD_ZELLIJ_BINDINGS" "false"
+        #                       }
+        #                   }
+        #               }
+        #           }
+        #       }
+        #   }
+        # '';
+      };
     };
   };
 }

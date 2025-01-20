a431fb48d23395578ee61c5a1ac892cca55700b7
bashfulrobot <dustin@bashfulrobot.com>
2025-01-20 15:50:58 -0800

    feat: Configure NFS server and system settings
    - Added configuration for the NFS server
    - Configured exports for the NFS server with multiple IP addresses
    - Set up a file system binding for `/exports/darkstar`
    - Enabled the NFS server
    - Added `nfs-srv.nix` to the list of imports in `default.nix`
    - Set the time zone to "America/Vancouver"
    - Set the default locale to "en_CA.UTF-8"
    - Configured X11 keymap layout to "us"
    - Enabled home-manager, insecure-packages, and nix-settings in the nixcfg
    - Commented out examples for enabling programs like mtr and gnupg agent
    - Removed firewall settings and left it disabled
    - Changed the system state version to "24.11"
    
    [systems/srv/config/nfs-srv.nix]
    - Added a new file `systems/srv/config/nfs-srv.nix`
    - Added a configuration for the NFS server
    - Set up a file system binding for `/exports/darkstar`
    - Enabled the NFS server
    - Configured exports for the NFS server with multiple IP addresses
    [systems/srv/config/default.nix]
    - Add `nfs-srv.nix` to the list of imports
    - Enable flakes and nix commands
    - Set the time zone to "America/Vancouver"
    - Set the default locale to "en_CA.UTF-8"
    - Configure X11 keymap layout to "us"
    - Enable home-manager, insecure-packages, and nix-settings in the nixcfg
    - Commented out examples for enabling programs like mtr and gnupg agent
    - Remove firewall settings and leave it disabled
    - Change the system state version to "24.11"

---
+++ systems/srv/config/nfs-srv.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, secrets, ... }: {
+
+  # environment.systemPackages = with pkgs; [ syncthing ];
+
+  fileSystems."/exports/darkstar" = {
+    device = "/home/dustin/nfs/darkstar";
+    options = [ "bind" ];
+  };
+
+  services = {
+    nfs = {
+      server = {
+        enable = true;
+        exports = ''
+          /exports/darkstar 172.16.166.10(rw,sync,no_subtree_check,root_squash) \
+                            172.16.166.11(rw,sync,no_subtree_check,root_squash) \
+                            172.16.166.12(rw,sync,no_subtree_check,root_squash) \
+                            172.16.166.20(rw,sync,no_subtree_check,root_squash) \
+                            172.16.166.21(rw,sync,no_subtree_check,root_squash)
+        '';
+      };
+    };
+  };
+}

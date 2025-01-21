3ef4d845ad85f2f405e878ecf2ffa5b8eb0ea36f
bashfulrobot <dustin@bashfulrobot.com>
2025-01-20 21:36:29 -0800

    chore: Change NFS mount options to 'sync'
    - Change the NFS mount options from `no_root_squash` to `sync` in [systems/srv/config/nfs-srv.nix]
    
    [systems/srv/config/nfs-srv.nix]
    - Change the NFS mount options from `no_root_squash` to `sync`

--- systems/srv/config/nfs-srv.nix
+++ systems/srv/config/nfs-srv.nix
@@ -12,11 +12,11 @@
       server = {
         enable = true;
         exports = ''
-          /exports/spitfire 172.16.166.10(rw,no_subtree_check,no_root_squash) \
-                            172.16.166.11(rw,no_subtree_check,no_root_squash) \
-                            172.16.166.12(rw,no_subtree_check,no_root_squash) \
-                            172.16.166.20(rw,no_subtree_check,no_root_squash) \
-                            172.16.166.21(rw,no_subtree_check,no_root_squash)
+          /exports/spitfire 172.16.166.10(rw,sync,no_subtree_check) \
+                            172.16.166.11(rw,sync,no_subtree_check) \
+                            172.16.166.12(rw,sync,no_subtree_check) \
+                            172.16.166.20(rw,sync,no_subtree_check) \
+                            172.16.166.21(rw,sync,no_subtree_check)
         '';
       };
     };

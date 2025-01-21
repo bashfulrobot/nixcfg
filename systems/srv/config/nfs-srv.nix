be79b5b3bb6593ab1f89bca91fa0c5df9fea54d4
bashfulrobot <dustin@bashfulrobot.com>
2025-01-20 16:20:41 -0800

    docs: Fix typo in NFS server configuration
    - Fix typo in exports list in [systems/srv/config/nfs-srv.nix] file
    
    [systems/srv/config/nfs-srv.nix]
    - Fix a typo in the exports list

--- systems/srv/config/nfs-srv.nix
+++ systems/srv/config/nfs-srv.nix
@@ -12,13 +12,13 @@
       server = {
         enable = true;
         exports = ''
-          /exports/spitfire 172.16.166.10(rw,sync,no_subtree_check,root_squash,insecure) \
-                            172.16.166.11(rw,sync,no_subtree_check,root_squash,insecure) \
-                            172.16.166.12(rw,sync,no_subtree_check,root_squash,insecure) \
-                            172.16.166.20(rw,sync,no_subtree_check,root_squash,insecure) \
-                            172.16.166.21(rw,sync,no_subtree_check,root_squash,insecure)
+          /exports/spitfire 172.16.166.10(rw,sync,,insecure,no_subtree_check,root_squash) \
+                            172.16.166.11(rw,sync,,insecure,no_subtree_check,root_squash) \
+                            172.16.166.12(rw,sync,,insecure,no_subtree_check,root_squash) \
+                            172.16.166.20(rw,sync,,insecure,no_subtree_check,root_squash) \
+                            172.16.166.21(rw,sync,,insecure,no_subtree_che,insecure,ck,root_squash)
         '';
-,insecure      };
+      };
     };
   };
 }

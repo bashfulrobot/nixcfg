167db23e70afc5ba3125d9310caab19dbcb9a6cd
bashfulrobot <dustin@bashfulrobot.com>
2025-01-22 12:30:12 -0800

    feat: Add keybindings and system packages for enhanced terminal experience
    - Enable the `blackbox` CLI tool and `services.envfs` feature in [suites/terminal/default.nix]
    - Add system packages for shell experience (fzf, tealdeer, bottom, jless, fd, sd, tree, broot, eza, btop) in [suites/terminal/default.nix]
    - Enable various programs like `autojump`, `zoxide`, `tmux`, `bat`, and `carapace` in [suites/terminal/default.nix]
    - Add configuration for Blackbox Terminal in [modules/cli/blackbox/default.nix]
    - Update keybindings in [modules/desktops/gnome/keybindings/default.nix] to use `blackbox` for opening Terminal, `chromium` for opening the browser, and `code` for opening the editor
    - Add new keybindings for annotating and performing OCR on screenshots, accessing 1Password, opening files in `~/dev`, transforming Gmail URLs, toggling cursor size in presentations, setting audio devices, opening 1Password flatpak, and saving currently playing song in NCSPOT to library in Spotify in [modules/desktops/gnome/keybindings/default.nix]
    
    [suites/terminal/default.nix]
    - Enable the `blackbox` CLI tool
    - Enable the `services.envfs` feature
    - Add several system packages for shell experience (fzf, tealdeer, bottom, jless, fd, sd, tree, broot, eza, btop)
    - Enable the `autojump` and `zoxide` programs in home-manager
    - Enable the `tmux` and `bat` programs in home-manager
    - Disable the `navi` program in home-manager
    - Enable the `carapace` program in home-manager
    [modules/cli/blackbox/default.nix]
    - Add a new file `modules/cli/blackbox/default.nix` with configuration for Blackbox Terminal
    [modules/desktops/gnome/keybindings/default.nix]
    - Updated the keybinding for opening Terminal to use `blackbox` instead of `alacritty`
    - Added a new keybinding for annotating screenshots using `/etc/profiles/per-user/dustin/bin/screenshot-annotate.sh`
    - Updated the keybinding for opening the browser to use `chromium` instead of the previous browser
    - Updated the keybinding for opening the editor to use `code` instead of the previous editor
    - Added a new keybinding for performing OCR on screenshots using `/etc/profiles/per-user/dustin/bin/screenshot-ocr.sh`
    - Added a new keybinding for quick access to 1Password using `/run/current-system/sw/bin/1password --quick-access`
    - Added a new keybinding for toggling 1Password using `/run/current-system/sw/bin/1password --toggle`
    - Added a new keybinding for opening files in `~/dev` using `/etc/profiles/per-user/dustin/bin/nautilus ~/dev`
    - Added a new keybinding for transforming Gmail URL to archive URL using `gmail-url`
    - Added a new keybinding for toggling large cursor in presentations using `toggle-cursor-size`
    - Added a new keybinding for setting audio devices using `set-audio-in-out`
    - Added a new keybinding for opening 1Password flatpak using `flatpak run com.onepassword.OnePassword`
    - Added a new keybinding for saving currently playing song in NCSPOT to library in Spotify using `ncspot-save-playing`

---
+++ modules/cli/blackbox/default.nix
@@ -0,0 +1,33 @@
+{ user-settings, pkgs, secrets, config, lib, inputs, ... }:
+let cfg = config.cli.blackbox;
+
+in {
+  options = {
+    cli.blackbox.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable Blackbox Terminal.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ blackbox-terminal ];
+
+    home-manager.users."${user-settings.user.username}" = {
+      dconf.settings = with inputs.home-manager.lib.hm.gvariant; {
+        "com/raggesilver/BlackBox" = {
+          cursor-blink-mode = mkUint32 1;
+          custom-working-directory = "/home/dustin/dev";
+          font = "JetBrainsMonoNL Nerd Font Mono 14";
+          scrollback-lines = mkUint32 50000;
+          terminal-padding =
+            mkTuple [ (mkUint32 15) (mkUint32 15) (mkUint32 15) (mkUint32 15) ];
+          window-height = mkUint32 734;
+          window-width = mkUint32 1215;
+          working-directory-mode = mkUint32 2;
+        };
+
+      };
+    };
+  };
+}

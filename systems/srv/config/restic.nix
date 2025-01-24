5ca065e8ea21015d8f34caff10421fe9ad99bee3
bashfulrobot <dustin@bashfulrobot.com>
2025-01-23 17:29:24 -0800

    chore: Add restic and autorestic to system packages and configure backup settings
    - Add `restic` and `autorestic` to `environment.systemPackages` in `systems/srv/config/restic.nix`
    - Add `.autorestic.yaml` file with snapshot settings and backup locations
    
    [systems/srv/config/restic.nix]
    - Modify the list of `environment.systemPackages` to include `restic` and `autorestic` on a single line
    - Add a `.autorestic.yaml` file with specific snapshot settings and backup locations

--- systems/srv/config/restic.nix
+++ systems/srv/config/restic.nix
@@ -1,10 +1,42 @@
 { user-settings, config, pkgs, secrets, ... }: {
 
-  environment.systemPackages = with pkgs; [
-      restic
-      autorestic
-    ];
+  environment.systemPackages = with pkgs; [ restic autorestic ];
   home-manager.users."${user-settings.user.username}" = {
+
+    home.file.".autorestic.yaml" = {
+      text = ''
+        version: 2
+
+        global:
+          forget:
+            keep-last: 5 # always keep at least 5 snapshots
+            keep-hourly: 3 # keep 3 last hourly snapshots
+            keep-daily: 4 # keep 4 last daily snapshots
+            keep-weekly: 1 # keep 1 last weekly snapshots
+            keep-monthly: 12 # keep 12 last monthly snapshots
+            keep-yearly: 2 # keep 7 last yearly snapshots
+            keep-within: '7d' # keep snapshots from the last 14 days
+
+        locations:
+          nfs:
+            from:
+              - /srv/nfs
+            to: b2-nfs
+            forget: prune
+            options:
+              exclude:
+                - /srv/nfs/lost+found
+                - /srv/nfs/spitfire/jellyfin
+
+        backends:
+          b2-nfs:
+            type: b2
+            path: 'srv-nfs'
+            env:
+              B2_ACCOUNT_ID: "${secrets.restic.B2_ACCOUNT_ID}"
+              B2_ACCOUNT_KEY: "${secrets.restic.B2_ACCOUNT_KEY}"
+      '';
+    };
 
   };
 }

1788d4753ecfe297a208d546da3db29e833c23a6
bashfulrobot <dustin@bashfulrobot.com>
2025-01-23 16:06:03 -0800

    chore: Refactor configuration file and environment variables
    - Remove environmentFile path from systems/srv/config/restic.nix
    - Add RESTIC_PASSWORD, B2_ACCOUNT_ID, B2_ACCOUNT_KEY to the environment in systems/srv/config/restic.nix
    
    [systems/srv/config/restic.nix]
    - Remove the environmentFile path since environment is used instead
    - Add RESTIC_PASSWORD, B2_ACCOUNT_ID, B2_ACCOUNT_KEY to the environment

--- systems/srv/config/restic.nix
+++ systems/srv/config/restic.nix
@@ -1,33 +1,20 @@
-{ config, pkgs, secrets, ... }:
-let
-  resticEnvContent = ''
-    RESTIC_REPOSITORY="${secrets.restic.srv.RESTIC_REPOSITORY}"
-    RESTIC_PASSWORD="${secrets.restic.srv.RESTIC_PASSWORD}"
-    B2_ACCOUNT_ID="${secrets.restic.srv.B2_ACCOUNT_ID}"
-    B2_ACCOUNT_KEY="${secrets.restic.srv.B2_ACCOUNT_KEY}"
-  '';
-in {
-  # Create the file in /etc
-  environment.etc."restic/env-srv-restic" = {
-    text = resticEnvContent;
-    mode = "0600"; # Set the file permissions to be readable only by root
-  };
-
-  # Configure restic to use the env file
+{ config, pkgs, secrets, ... }: {
   services.restic.backups = {
     b2backup = {
       initialize = true;
       paths = [ "/srv/nfs" ];
-      environmentFile = "/etc/restic/env-srv-restic";
       repository = "${secrets.restic.srv.RESTIC_REPOSITORY}";
       pruneOpts = [ "--keep-daily 7" "--keep-weekly 4" "--keep-monthly 6" ];
-      extraBackupArgs = [
-        "--exclude=/srv/nfs/spitfire/jellyfin"
-      ];
+      extraBackupArgs = [ "--exclude=/srv/nfs/spitfire/jellyfin" ];
       timerConfig = {
         OnCalendar = "03:00:00";
         RandomizedDelaySec = "5m";
         Persistent = true;
+      };
+      environment = {
+        RESTIC_PASSWORD = "${secrets.restic.srv.RESTIC_PASSWORD}";
+        B2_ACCOUNT_ID = "${secrets.restic.srv.B2_ACCOUNT_ID}";
+        B2_ACCOUNT_KEY = "${secrets.restic.srv.B2_ACCOUNT_KEY}";
       };
     };
   };

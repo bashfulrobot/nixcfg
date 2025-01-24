3a5970e5b168e78308ac90a93d7f8e468006ddbe
bashfulrobot <dustin@bashfulrobot.com>
2025-01-23 16:50:08 -0800

    Update system configuration and pass setup
    - Add cribs notes for setting up a pass in `helpers/reference/how/pass.md`
    - Remove `services.restic.backups` block in `systems/srv/config/restic.nix`
    - Add `restic` and `autorestic` to `systemPackages` in `systems/srv/config/restic.nix`
    - Remove `.config/restic/srv-nfs-env` and `.config/restic/srv-nfs-password` files from `home-manager.users."${user-settings.user.username}"` in `systems/srv/config/restic.nix`
    - Add `gnupg2` to the list of `systemPackages` in `systems/srv/config/software.nix`
    
    [helpers/reference/how/pass.md]
    - Add `helpers/reference/how/pass.md` with cribs notes about setting up a pass
    - Cribs notes include steps for setting up pass after a fresh install
    [systems/srv/config/restic.nix]
    - Remove the `services.restic.backups` block
    - Add the dependencies `restic` and `autorestic` to `systemPackages`
    - Remove the `.config/restic/srv-nfs-env` and `.config/restic/srv-nfs-password` files from `home-manager.users."${user-settings.user.username}"`
    - The rest of the changes are not relevant for the summary
    [systems/srv/config/software.nix]
    - Added `gnupg2` to the list of systemPackages

--- systems/srv/config/restic.nix
+++ systems/srv/config/restic.nix
@@ -1,31 +1,7 @@
 { user-settings, config, pkgs, secrets, ... }: {
-  services.restic.backups = {
-    b2backup = {
-      environmentFile = "/home/${user-settings.user.username}/.config/restic/srv-nfs-env";
-      passwordFile = "/home/${user-settings.user.username}/.config/restic/srv-nfs-password";
-      initialize = true;
-      paths = [ "/srv/nfs" ];
-      repository = "${secrets.restic.srv.RESTIC_REPOSITORY}";
-      pruneOpts = [ "--keep-daily 7" "--keep-weekly 4" "--keep-monthly 6" ];
-      extraBackupArgs = [ "--exclude=/srv/nfs/spitfire/jellyfin" ];
-      timerConfig = {
-        OnCalendar = "03:00:00";
-        RandomizedDelaySec = "5m";
-        Persistent = true;
-      };
-    };
-  };
 
+  systemPackages = with pkgs; [ restic autorestic ];
   home-manager.users."${user-settings.user.username}" = {
-    home.file.".config/restic/srv-nfs-env" = {
-      text = ''
-        B2_ACCOUNT_ID=${secrets.restic.srv.B2_ACCOUNT_ID}
-        B2_ACCOUNT_KEY=${secrets.restic.srv.B2_ACCOUNT_KEY}
-      '';
-    };
-    home.file.".config/restic/srv-nfs-password" = {
-      text = "${secrets.restic.srv.RESTIC_PASSWORD}";
-    };
 
   };
 }

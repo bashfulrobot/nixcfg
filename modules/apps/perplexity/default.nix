60a0fc18dc2fc8e1b1e4a4be659300c8e8efde33
bashfulrobot <dustin@bashfulrobot.com>
2025-01-24 15:04:25 -0800

    feat: Add Perplexity App
    [modules/apps/perplexity/icons/*]
    - Added a new file `modules/apps/perplexity/icons/256.png`
    - Added a new file: `modules/apps/perplexity/icons/128.png`
    - Add new file `modules/apps/perplexity/icons/96.png`
    - Add new file `modules/apps/perplexity/icons/64.png`
    - Added a new file `modules/apps/perplexity/icons/48.png`
    - Add a new file `modules/apps/perplexity/icons/32.png`
    - Add a new file `modules/apps/perplexity/icons/16.png`
    
    [suites/utilities/default.nix]
    - Enable the `perplexity` app in `suites/utilities/default.nix`
    
    [modules/apps/perplexity/default.nix]
    - Add a new file `modules/apps/perplexity/default.nix`
    - Set `apps.perplexity.enable` to `false` by default
    - Add `perplexityApp.desktopItem` and `perplexityApp.icons` to `environment.systemPackages`

---
+++ modules/apps/perplexity/default.nix
@@ -0,0 +1,43 @@
+# https://raw.githubusercontent.com/Gako358/dotfiles/6236f706279d2450606dfc99fecce9399936b7e7/home/programs/browser/teams.nix
+{ user-settings, config, pkgs, lib, makeDesktopItem, ... }:
+
+let
+  cfg = config.apps.perplexity;
+
+  # Import the makeDesktopApp function
+  makeDesktopApp = pkgs.callPackage ../../../lib/cbb-webwrap { };
+
+  # I temp create an app in brave to download all the icons, then I place then in the correct folder
+  perplexityApp = makeDesktopApp {
+    name = "Perplexity";
+    url = "https://perplexity.ai";
+    binary = "${pkgs.chromium}/bin/chromium";
+    myStartupWMClass = "perplexity.ai";
+    iconSizes = [ "16" "32" "48" "64" "96" "128" "256" ];
+    # iconSizes = [ "256" ]; # forcing large icon use
+    iconPath = ./icons; # path to icons
+    # Open In Browser vs Open as App
+    useAppFlag = true;
+  };
+
+in {
+
+  options = {
+    apps.perplexity.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable the perplexity app.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    environment.systemPackages = [ perplexityApp.desktopItem ]
+      ++ perplexityApp.icons;
+
+    # home-manager.users."${user-settings.user.username}" = {
+
+    # };
+  };
+
+}

3132bcd7dccaacdebf0086982bcefd054a0959ad
bashfulrobot <dustin@bashfulrobot.com>
2025-01-24 14:29:03 -0800

    chore: Update shellAliases to watch youtube videos using mpv
    [modules/cli/fish/default.nix]
    - Removed the `youtube` alias from the `shellAliases` section
    - Updated the `shellAliases` section to include a new alias `youtube` for running `nix run nixpkgs#mpv`

--- modules/cli/fish/default.nix
+++ modules/cli/fish/default.nix
@@ -358,6 +358,7 @@ nvim $filename
           "..." = "cd ../..";
           "...." = "cd ../../..";
           "....." = "cd ../../../..";
+          youtube = "nix run nixpkgs#mpv";
           sysdig-cli-scanner-recent-version =
             "curl -L -s https://download.sysdig.com/scanning/sysdig-cli-scanner/latest_version.txt";
           sysdig-cli-scanner-get = ''

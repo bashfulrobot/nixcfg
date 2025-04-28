5594b1b0e14dbe5163aafeba870264ec422b7829
bashfulrobot <dustin@bashfulrobot.com>
2025-04-28 09:10:37 -0700

    ✨ feat(espanso): add text expansion support with common shortcuts
    Added Espanso text expansion tool configuration with several useful
    shortcuts including emoticons (:shrug, :tflip, :fingerguns), email
    addresses, and date insertion. Created a basic setup with a shell script
    to install additional packages. Enabled the module in the Darwin system
    configuration to provide quick text expansion capabilities across the
    system.

---
+++ modules/cli/espanso/default.nix
@@ -0,0 +1,64 @@
+{ user-settings, pkgs, config, lib, ... }:
+let cfg = config.cli.espanso;
+in {
+  options = {
+    cli.espanso.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable Espanso.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    # environment.systemPackages = with pkgs; [  ];
+
+    home-manager.users."${user-settings.user.username}" = {
+      services.espanso = {
+        enable = true;
+        matches = {
+          base = {
+            matches = [
+              {
+                trigger = ":shrug";
+                replace = "¯\\_(ツ)_/¯";
+              }
+              {
+                trigger = ":tflip";
+                replace = "(╯°□°）╯︵ ┻━┻";
+              }
+
+              {
+                trigger = ":fingerguns";
+                replace = "(☞ ͡° ͜ʖ ͡°)☞";
+              }
+            ];
+          };
+          personal = {
+            matches = [
+              {
+                trigger = ":wem";
+                replace = "dustin.krysak@sysdig.com";
+              }
+              {
+                trigger = ":pem";
+                replace = "dustin@bashfulrobot.com";
+              }
+            ];
+          };
+          util = {
+            matches = [{
+              trigger = ":date";
+              replace = "{{currentdate}}";
+              vars = [{
+                name = "currentdate";
+                type = "date";
+                params.format = "%Y-%m-%d";
+              }];
+            }];
+          };
+        };
+      };
+
+    };
+  };
+}
---
+++ modules/cli/espanso/packages.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#
+# Until I figure out how to declare in nix.
+#
+
+espanso install basic-emojis

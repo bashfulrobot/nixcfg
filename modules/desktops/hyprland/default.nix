bc96e0136198e62639d74f788a639e4e68d0372c
bashfulrobot <dustin@bashfulrobot.com>
2025-05-13 23:59:52 -0700

    ✨ feat: integrate hyprflake for Hyprland configuration
    Adds hyprflake as a flake input and integrates it with NixOS and
    Home Manager modules. Simplifies the Hyprland configuration by removing
    manual package management and replacing it with hyprflake's modules.
    Also fixes a typo in the Hyprland description and renames the 'desktops'
    option to 'desktop' for consistency.

--- modules/desktops/hyprland/default.nix
+++ modules/desktops/hyprland/default.nix
@@ -1,35 +1,20 @@
-# If you start experiencing lag and FPS drops in games or programs like Blender on stable NixOS when using the Hyprland flake, it is most likely a mesa version mismatch between your system and Hyprland.
-# - https://wiki.hyprland.org/Nix/Hyprland-on-NixOS/
-# You can fix this issue by using mesa from Hyprland’s nixpkgs input
-
-{ user-settings, pkgs, inputs, config, lib, ... }:
-let cfg = config.desktops.hyprland;
+{ config, pkgs, lib, user-settings, ... }:
+let cfg = config.desktop.hyprland;
 in {
+
   options = {
-    desktops.hyprland.enable = lib.mkOption {
+    desktop.hyprland.enable = lib.mkOption {
       type = lib.types.bool;
       default = false;
-      description = "Enable Hypland.";
+      description = "Enable Hyprland..";
     };
   };
 
   config = lib.mkIf cfg.enable {
 
-    programs = {
-      hyprland = {
-        enable = true;
+    environment.systemPackages = with pkgs; [
 
-        # set the flake package
-        package =
-          inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-        # make sure to also set the portal package, so that they are in sync
-        portalPackage =
-          inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-      };
-    };
-
-    environment.systemPackages = with pkgs; [ wl-clipboard ];
-    # TODO: Update version periodically
+    ];
 
     home-manager.users."${user-settings.user.username}" = {
 

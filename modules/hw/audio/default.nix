341a05c2afa2573c5fac2057e8daba58ce93cca6
bashfulrobot <dustin@bashfulrobot.com>
2025-05-27 13:51:29 -0700

    🔧 fix(audio): move pulseaudio config from hardware to services namespace

--- modules/hw/audio/default.nix
+++ modules/hw/audio/default.nix
@@ -1,6 +1,13 @@
-{ pkgs, config, lib, ... }:
-let cfg = config.hw.audio;
-in {
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.hw.audio;
+in
+{
 
   options = {
     hw.audio.enable = lib.mkOption {
@@ -11,28 +18,30 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    # Enable sound with pipewire.
-    hardware.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-      # needed for hyprland screensharing
-      # wireplumber.enable = true;
 
-      # use the example session manager (no others are packaged yet so this is enabled by default,
-      # no need to redefine it in your config for now)
-      #media-session.enable = true;
+    security.rtkit.enable = true;
+    services = {
+      # Enable sound with pipewire.
+      pulseaudio.enable = false;
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # If you want to use JACK applications, uncomment this
+        #jack.enable = true;
+        # needed for hyprland screensharing
+        # wireplumber.enable = true;
+
+        # use the example session manager (no others are packaged yet so this is enabled by default,
+        # no need to redefine it in your config for now)
+        #media-session.enable = true;
+      };
     };
 
-    environment.systemPackages = with pkgs;
-      [
+    environment.systemPackages = with pkgs; [
 
-      ];
+    ];
   };
 }
 # sudo alsactl store - fixes  error: failed to import hw:0 use case configuration -2

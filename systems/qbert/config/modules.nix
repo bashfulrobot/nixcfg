5208ed06976e4d8a5f23b2bf6b9122fd841fc39e
bashfulrobot <dustin@bashfulrobot.com>
2025-06-03 21:41:45 -0700

    ✨ feat: add module to disable systemd-resolved DNS stub listener

--- systems/qbert/config/modules.nix
+++ systems/qbert/config/modules.nix
@@ -1,10 +1,13 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+{
 
   users.default.enable = true;
 
   sys.desktop-files = {
     enable = true;
     reboot-windows = true;
+
+    disable-stub-dns.enable = true;
   };
 
   # Enable gnome desktop

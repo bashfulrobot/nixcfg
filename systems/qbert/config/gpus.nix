8654e195d8979b70b4b930020e51bdf04fa511fc
bashfulrobot <dustin@bashfulrobot.com>
2025-06-04 19:55:42 -0700

    🔧 refactor(gpus.nix): restructure boot configuration for AMD GPU

--- systems/qbert/config/gpus.nix
+++ systems/qbert/config/gpus.nix
@@ -1,13 +1,21 @@
 { config, pkgs, ... }:
 {
   # The kernel can load the correct driver right away:
-  boot.initrd.kernelModules = [ "amdgpu" ];
+  boot = {
+    initrd.kernelModules = [ "amdgpu" ];
+    # tshooting desktop lockups
+    # kernelParams = [
+    #   "amdgpu.dc=0" # Disable Display Core
+    #   "amdgpu.gfxoff=0" # Disable power gating
+    # ];
+  };
   # Make sure Xserver uses the `amdgpu` driver
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "amdgpu" ];
   hardware.graphics = {
-      # For 32 bit applications
-      enable32Bit = true;
-      enable = true;
-    };
+    # For 32 bit applications
+    enable32Bit = true;
+    enable = true;
+  };
+
 }

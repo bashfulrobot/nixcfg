7bb22819e310c6f5e977ef1527195ba419071cf7
bashfulrobot <dustin@bashfulrobot.com>
2025-06-13 03:14:56 -0700

    🔧 feat(hw): enhance firmware and bluetooth configuration
    Improve hardware configuration by enabling all firmware including unfree,
    adding udev service, and configuring UEFI capsule update settings for
    better hardware support and firmware updates

---
+++ modules/hw/bluetooth/default.nix
@@ -0,0 +1,31 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.hw.bluetooth;
+in
+{
+
+  options = {
+    hw.bluetooth.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable bluetooth hardware.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    hardware.bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      settings = {
+        Policy = {
+          autoEnable = true;
+        };
+      };
+    };
+  };
+}

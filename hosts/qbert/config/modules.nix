f77e2615067dd2c1dc96779a188dd5fa735033c1
bashfulrobot <dustin@bashfulrobot.com>
2025-06-13 03:12:28 -0700

    ✨ feat(qbert): add NixOS configuration for new host
    Add initial NixOS configuration for host "qbert" with AMD GPU support,
    disk partitioning setup, boot configuration, and networking settings.
    The configuration includes Hyprland desktop environment and various
    system modules.

---
+++ hosts/qbert/config/modules.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+{
+
+  users.default.enable = true;
+
+  sys = {
+    desktop-files = {
+      enable = true;
+      reboot-windows = true;
+
+    };
+    disable-stub-dns.enable = true;
+  };
+
+  # Enable gnome desktop
+  desktops.gnome.enable = false;
+  desktops.tiling.hyprland.enable = true;
+
+  apps.syncthing = {
+    enable = true;
+    host.qbert = true;
+  };
+}

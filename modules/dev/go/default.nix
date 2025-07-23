f53b09080a5c1c423e50e8678d0910d436275bcd
bashfulrobot <dustin@bashfulrobot.com>
2025-07-23 12:09:10 -0700

    ✨ feat(dev): add golang module

---
+++ modules/dev/go/default.nix
@@ -0,0 +1,23 @@
+{ user-settings, pkgs, config, lib, ... }:
+let cfg = config.dev.go;
+
+in {
+  options = {
+    dev.go.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable go tooling.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    environment.systemPackages = with pkgs; [
+
+      unstable.go
+    ];
+    home-manager.users."${user-settings.user.username}" = {
+
+    };
+  };
+}

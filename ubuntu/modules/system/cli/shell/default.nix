76fb2103e6e72ab3a86cc0602d5a676e03ee0fbb
bashfulrobot <dustin@bashfulrobot.com>
2025-08-02 11:45:50 -0700

    🔧 fix(ubuntu): simplify shell module to always add fish to /etc/shells
    - Remove conditional logic that referenced home-manager config
    - Always include fish in /etc/shells for system-manager
    - Fixes build error where config.cli.fish.enable was not available in system scope

--- ubuntu/modules/system/cli/shell/default.nix
+++ ubuntu/modules/system/cli/shell/default.nix
@@ -2,19 +2,13 @@
 
 {
   config = {
-    # Automatically add enabled shells to /etc/shells
-    environment.etc."shells".text = lib.concatStringsSep "\n" (
-      [
-        "/bin/sh"
-        "/bin/bash"
-        "/usr/bin/bash"
-      ] ++ lib.optionals config.cli.fish.enable [
-        "${pkgs.fish}/bin/fish"
-      ]
-      # Future shells can be added here with similar conditionals
-      # ++ lib.optionals config.cli.zsh.enable [
-      #   "${pkgs.zsh}/bin/zsh"
-      # ]
-    );
+    # Always add common shells and fish to /etc/shells
+    environment.etc."shells".text = lib.concatStringsSep "\n" [
+      "/bin/sh"
+      "/bin/bash" 
+      "/usr/bin/bash"
+      "${pkgs.fish}/bin/fish"
+      # Future shells can be added here
+    ];
   };
 }

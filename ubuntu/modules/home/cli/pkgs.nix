286da4dc6753d9fa16eae4a48d85c40dc0d9468e
bashfulrobot <dustin@bashfulrobot.com>
2025-08-04 17:09:49 -0700

    🗑️ remove(ubuntu): remove wcurl package from CLI tools

--- ubuntu/modules/home/cli/pkgs.nix
+++ ubuntu/modules/home/cli/pkgs.nix
@@ -1,9 +1,13 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 # General CLI packages for home-manager installation
 
 {
   home.packages = with pkgs; [
-    wcurl  # HTTP client with improved output
     # Note: atuin handled by programs.atuin.enable in fish.nix
   ];
 }

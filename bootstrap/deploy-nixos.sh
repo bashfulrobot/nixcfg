330f1fafe54c86fc97b1f3e992e65a0526e88e5c
bashfulrobot <dustin@bashfulrobot.com>
2025-08-07 13:19:54 -0700

    🔧 fix: correct nixos-install variable expansion
    Fix variable expansion in nixos-install flake reference using ${SYSTEM_NAME} syntax to properly resolve system name during installation.

--- bootstrap/deploy-nixos.sh
+++ bootstrap/deploy-nixos.sh
@@ -245,9 +245,11 @@ nix-shell -p git git-crypt wget curl --run "
     
     # Install the system
     echo
-    echo -e '${BLUE}Installing NixOS system: \$SYSTEM_NAME${NC}'
+    echo -e '${BLUE}Installing NixOS system: '\$SYSTEM_NAME'${NC}'
+    echo -e '${BLUE}Current directory: '\$(pwd)'${NC}'
+    ls -la flake.nix || echo 'flake.nix not found in current directory'
     ulimit -n 4096
-    nixos-install --flake \".\#\$SYSTEM_NAME\" --impure --no-root-passwd
+    nixos-install --flake \".\#\${SYSTEM_NAME}\" --impure --no-root-passwd
     
     echo
     echo -e '${GREEN}Installation complete!${NC}'

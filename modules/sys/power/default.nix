1b69f2c86ae9e97c3e9417b605ef437868b1b4ce
bashfulrobot <dustin@bashfulrobot.com>
2025-08-21 14:46:11 -0700

    ✨ feat(power): add clamshell mode for donkeykong
    - extend power module with clamshell option
    - disable suspend on lid close when on external power
    - disable all suspend modes (suspend, hibernation, hybrid sleep)
    - enable clamshell mode for donkeykong host

--- modules/sys/power/default.nix
+++ modules/sys/power/default.nix
@@ -8,18 +8,40 @@ in {
       default = false;
       description = "Enable laptop power tools.";
     };
+    
+    sys.power.clamshell = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable clamshell mode - disable suspend on lid close.";
+    };
   };
 
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      brightnessctl
-      light
-    ];
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      environment.systemPackages = with pkgs; [
+        brightnessctl
+        light
+      ];
 
-    services.power-profiles-daemon.enable = true;
+      services.power-profiles-daemon.enable = true;
 
-    # home-manager.users."${user-settings.user.username}" = {
+      # home-manager.users."${user-settings.user.username}" = {
 
-    # };
-  };
+      # };
+    })
+    
+    (lib.mkIf cfg.clamshell {
+      # Prevent the laptop from going to sleep when the lid closes
+      # Only works if user is logged in
+      services.logind.lidSwitchExternalPower = "ignore";
+
+      # Disable all suspend modes
+      systemd.sleep.extraConfig = ''
+        AllowSuspend=no
+        AllowHibernation=no
+        AllowHybridSleep=no
+        AllowSuspendThenHibernate=no
+      '';
+    })
+  ];
 }

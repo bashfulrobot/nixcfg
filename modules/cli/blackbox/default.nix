7746af8ae9e0d143a9b7834850468d86e5588efe
bashfulrobot <dustin@bashfulrobot.com>
2025-08-22 14:00:06 -0700

    refactor(blackbox): ♻️ Update settings via dconf
    - set cursor-shape to block
    - set show-headerbar to true

--- modules/cli/blackbox/default.nix
+++ modules/cli/blackbox/default.nix
@@ -17,7 +17,9 @@ in {
       dconf.settings = with inputs.home-manager.lib.hm.gvariant; {
         "com/raggesilver/BlackBox" = {
           cursor-blink-mode = mkUint32 1;
+          cursor-shape = uint32 0;
           custom-working-directory = "/home/dustin/dev";
+          show-headerbar = true;
           font = "JetBrainsMonoNL Nerd Font Mono 14";
           scrollback-lines = mkUint32 50000;
           terminal-padding =

e4a8c5e5f073638280476a23cd9510d4ca69ed25
bashfulrobot <dustin@bashfulrobot.com>
2025-09-08 15:06:57 -0700

    feat(kong-docs): ✨ add Kong Developer Documentation web wrapper
    - Create kong-docs module for https://developer.konghq.com/
    - Configure Chrome web app with predicted window class
    - Include Kong logo icons across all required sizes (16px-512px)
    - Provides branded access to Kong's developer documentation

---
+++ modules/apps/kong-docs/default.nix
@@ -0,0 +1,37 @@
+# Kong Developer Documentation web app
+{ user-settings, config, pkgs, lib, makeDesktopItem, ... }:
+
+let
+  cfg = config.apps.kong-docs;
+
+  # Import the makeDesktopApp function
+  makeDesktopApp = pkgs.callPackage ../../../lib/cbb-webwrap { };
+
+  kongDocsApp = makeDesktopApp {
+    name = "Kong Docs";
+    url = "https://developer.konghq.com/";
+    binary = "${pkgs.google-chrome}/bin/google-chrome-stable";
+    myStartupWMClass = "chrome-developer.konghq.com__-Default";
+    iconSizes = [ "16" "32" "48" "64" "96" "128" "192" "256" "512" ];
+    iconPath = ./icons;
+    useAppFlag = true;
+  };
+
+in {
+
+  options = {
+    apps.kong-docs.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable the Kong Developer Documentation app.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    environment.systemPackages = [ kongDocsApp.desktopItem ]
+      ++ kongDocsApp.icons;
+
+  };
+
+}

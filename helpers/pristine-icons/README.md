e6f1194b766e2bc4c8eea2c79338c7355874859c
bashfulrobot <dustin@bashfulrobot.com>
2025-09-09 08:46:36 -0700

    feat(apps): ✨ add Lucidchart web wrapper module
    - Add new lucidchart app module with web wrapper configuration
    - Include pristine icons in various sizes (16-512px)
    - Generate branded icons with work overlay in bottom-right
    - Configure Chrome startup class for proper window management
    - Target URL: https://lucid.app/documents#/home?folder_id=recent

--- helpers/pristine-icons/README.md
+++ helpers/pristine-icons/README.md
@@ -1,1 +1,2 @@
 <https://www.cleanpng.com/> seems to have many logos.
+<https://www.stickpng.com/> seems to have more
---
+++ modules/apps/lucidchart/default.nix
@@ -0,0 +1,37 @@
+# Lucidchart - Diagramming and visualization app
+{ user-settings, config, pkgs, lib, makeDesktopItem, ... }:
+
+let
+  cfg = config.apps.lucidchart;
+
+  # Import the makeDesktopApp function
+  makeDesktopApp = pkgs.callPackage ../../../lib/cbb-webwrap { };
+
+  lucidchartApp = makeDesktopApp {
+    name = "Lucidchart";
+    url = "https://lucid.app/documents#/home?folder_id=recent";
+    binary = "${pkgs.google-chrome}/bin/google-chrome-stable";
+    myStartupWMClass = "chrome-lucid.app__documents-Default";
+    iconSizes = [ "16" "32" "48" "64" "96" "128" "192" "256" "512" ];
+    iconPath = ./icons;
+    useAppFlag = true;
+  };
+
+in {
+
+  options = {
+    apps.lucidchart.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable the Lucidchart app.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    environment.systemPackages = [ lucidchartApp.desktopItem ]
+      ++ lucidchartApp.icons;
+
+  };
+
+}

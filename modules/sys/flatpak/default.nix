5882de422981a2b3590ab4486859408dc0f85b62
bashfulrobot <dustin@bashfulrobot.com>
2025-10-10 12:34:59 -0700

    refactor(flatpak): ♻️ restructure service configuration
    Reorganize flatpak service configuration into nested structure for
    better readability and maintainability.

--- modules/sys/flatpak/default.nix
+++ modules/sys/flatpak/default.nix
@@ -12,27 +12,29 @@ in {
   config = lib.mkIf cfg.enable {
     # https://github.com/gmodena/nix-flatpak
 
-    # Enable Flatpaks
-    services.flatpak.enable = true;
-    
-    xdg.portal.xdgOpenUsePortal = true;
-
-    # enable updates at system activation (default false)
-    # services.flatpak.update.onActivation = true;
-
-    # Update flatpaks weekly
-    services.flatpak.update.auto = {
+    # Flatpak configuration
+    services.flatpak = {
       enable = true;
-      onCalendar = "weekly"; # Default value
+
+      # enable updates at system activation (default false)
+      # update.onActivation = true;
+
+      # Update flatpaks weekly
+      update.auto = {
+        enable = true;
+        onCalendar = "weekly"; # Default value
+      };
+
+      # By default nix-flatpak will add the flathub remote. Remotes can be manually configured via the services.flatpak.remotes option:
+      #   remotes = [{
+      #   name = "flathub-beta"; location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+      # }];
+
+      packages = [
+        "com.github.tchx84.Flatseal"  # Flatpak permission manager
+      ];
     };
 
-    # By default nix-flatpak will add the flathub remote. Remotes can be manually configured via the services.flatpak.remotes option:
-    #   services.flatpak.remotes = [{
-    #   name = "flathub-beta"; location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
-    # }];
-
-    services.flatpak.packages = [
-      "com.github.tchx84.Flatseal"  # Flatpak permission manager
-    ];
+    xdg.portal.xdgOpenUsePortal = true;
   };
 }
